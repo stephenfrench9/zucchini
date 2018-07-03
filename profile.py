a049a2dacf32731af20095997b27d825cd987cc9
stephenfrench9 <stephenfrench9@gmail.com>
2018-07-02 17:01:01 -0700

    clean working master branch, runs on a cpu

--- profile.py
+++ profile.py
@@ -6,26 +6,10 @@ from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import torch
 
-def init_weights(m):
-    print("Here Is One Module:")
-    print("Type: ")
-    print(type(m))
-    print()
-    print(m.weight.data)
-    print("now modify it")
-    torch.Tensor.normal_(m.weight.data)
-    print(m.weight.data)
-    input("Ready for the next one?")
-    if type(m) == torch.nn.Linear:
-        print("We got a weird type match!!!")
-    #    m.weight.data.fill_(1.0)
-    #    print(m.weight)
-
-
 def fit_data(N, din, dowt, dhidden, lr, epochs, device):
-
-    print("Fittinig a Brand Shiny New Shiny shiny new neural net")
-# apply(init_weights)
+    """
+    generate new model, generate new data. Fit it.
+    """
     f = twoTransformations(din, dhidden, dowt)
     x = torch.randn(N, din, device = device)
     y = handle_all_trials(x, N, din, dowt, device)
@@ -38,7 +22,7 @@ def fit_data(N, din, dowt, dhidden, lr, epochs, device):
 
 def run_all(sizes, din, dowt, dhidden, lr, epochs, device):
     """
-    Find the test loss slope for each N in sizes
+    Find the test loss slope for each 'N' in sizes.
     """
     slopes = []
     for inx, N in enumerate(sizes):
@@ -51,21 +35,17 @@ def run_all(sizes, din, dowt, dhidden, lr, epochs, device):
        
 if __name__ == '__main__':
     """
-    finds the slope for all input shapes
+    Finds the slope for all input shapes
     """
-    din, dhidden, dowt = 10, 5, 2
-    d = twoTransformations(din, dhidden, dowt)
-#    d.cuda()
-    
-#    device = torch.device("cuda:0")
-    device = torch.device("cpu")
-    
     din, dowt = 10, 2 #Data shape
     dhidden = 5 #neural network architechture
     lr, epochs = .001, 5001 #Training parameters
-   
-    sizes = [25*i+25 for i in range(3)]
+    d = twoTransformations(din, dhidden, dowt) #dummy module
+#    d.cuda()
+#    device = torch.device("cuda:0") #choose hardware
+    device = torch.device("cpu")
 
+    sizes = [25*i+25 for i in range(3)]
     slopes = run_all(sizes, din, dowt, dhidden, lr, epochs, device)
 
     plt.plot(sizes, slopes)
@@ -73,5 +53,5 @@ if __name__ == '__main__':
               .format(lr, epochs))
     plt.xlabel("Trials: x, din: {}, dowt: {}".format(din, dowt))
     plt.ylabel("Slope: y, dhidden: {}".format(dhidden))
-    plt.savefig("thisis")
+    plt.savefig("nnetworks")
     
